6673b1fdc9235c7249451d554310c2a70896e03b
Anastasia Krasnova <a.dkrasnova@yandex.ru>
2025-03-14 13:21:47 +0300

    feat: improve input validation to restrict invalid values

--- main.js
+++ main.js
@@ -7,6 +7,34 @@ const hideToggleCheckbox = document.querySelector("#hideToggle");
 const progressContainer = document.querySelector("#progressBar");
 const progress = new Progress(progressContainer);
 
+progressValueInput.addEventListener("keydown", (event) => {
+  if (
+    event.key === "Backspace" ||
+    event.key === "Delete" ||
+    event.key === "ArrowLeft" ||
+    event.key === "ArrowRight" ||
+    event.key === "Enter"
+  )
+    return;
+
+  if (isNaN(Number(event.key))) {
+    event.preventDefault();
+    return;
+  }
+
+  const { selectionStart, selectionEnd } = event.target;
+  const currentValue = event.target.value;
+
+  const newValue =
+    currentValue.slice(0, selectionStart) +
+    event.key +
+    currentValue.slice(selectionEnd);
+
+  if (Number(newValue) > 100 || Number(newValue) < 0) {
+    event.preventDefault();
+  }
+});
+
 progressValueInput.addEventListener("change", (event) => {
   progress.setProgress(event.target.value);
 });
